77a3e2a328eb104b22b54ff84489c2f38067a055
kiranrishika <44136821+kiranrishika@users.noreply.github.com>
2019-03-10 19:09:52 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,17 @@
+import java.io.*;
+importjava.util.Scanner;
+importjava.lang.Math;
+public class first{
+	public static void main(String args[])
+	{
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the cartesian point");
+	intx,y,dist,r,t;
+	x=in.nextInt();
+	y=in.nextInt();
+r=in.nextInt();
+t= in.nextInt();
+	dist=Math.abs(r-x)+Math.abs(t-y);
+	System.out.println("the distance is "+dist);
+	}
+}
